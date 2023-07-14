ea00e7d4a40300989b3f0ba632552ce7fde97368
baaserosman <baaser.osman@gmail.com>
2023-07-14 10:45:43 +0200

    firstcommit

---
+++ flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def head():
+    return render_template('index.html', number1=2546, number2=7876)
+
+
+@app.route('/mult')
+
+def number():
+    x=54
+    y=58
+    return render_template('body.html', num1=x, num2=y, mult=x*y)
+
+if __name__ == '__main__':
+    app.run(debug=True)
